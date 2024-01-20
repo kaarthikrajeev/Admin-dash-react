7c0c295c6bad0a54c77420a911354b4db20a0af9
kaarthikraj <karthick77r@gmail.com>
2024-01-20 22:35:44 +0530

    components file setup

--- src/App.js
+++ src/App.js
@@ -25,6 +25,31 @@ const App = () => {
                 }>
                     <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>Navbar</div>
                 </div>
+                <div>
+                    <Routes>
+                        {/* Dashboard */}
+                         <Route path='/' element="Ecommerce"/>
+                         <Route path='/ecommerce' element="Ecommerce"/>
+                         {/* Pages */}
+                         <Route path='/orders' element="Orders"/>
+                         <Route path='/employees' element="Employees"/>
+                         <Route path='/customers' element="Customers"/>
+                         {/* Apps */}
+                         <Route path='/kanban' element="Kanban"/>
+                         <Route path='/editor' element="Editor"/>
+                         <Route path='/calender' element="Calender"/>
+                         <Route path='/color-picker' element="colorPicker"/>
+                         {/* Charts */}
+                         <Route path='/line' element="Line"/>
+                         <Route path='/area' element="Area"/>
+                         <Route path='/bar' element="bar"/>
+                         <Route path='/pie' element="pie"/>
+                         <Route path='/financial' element="Financial"/>
+                         <Route path='/color-mapping' element="ColorMapping"/>
+                         <Route path='/pyramid' element="pyramid"/>
+                         <Route path='/stacked' element="stacked"/>
+                    </Routes>
+                </div>
         </div>
         </BrowserRouter>
     </div>
